06cddf6920afd14b084396260cbd9bec413ed326
Nuke <nuke-web3@proton.me>
2025-04-02 15:20:57 -0600

    dummy rename to chacha & start rm of unneeded bits

--- script/src/bin/evm.rs
+++ script/src/bin/evm.rs
@@ -10,9 +10,7 @@
 //! RUST_LOG=info cargo run --release --bin evm -- --system plonk
 //! ```
 
-use alloy_sol_types::SolType;
 use clap::{Parser, ValueEnum};
-use fibonacci_lib::PublicValuesStruct;
 use serde::{Deserialize, Serialize};
 use sp1_sdk::{
     include_elf, HashableKey, ProverClient, SP1ProofWithPublicValues, SP1Stdin, SP1VerifyingKey,
@@ -20,7 +18,7 @@ use sp1_sdk::{
 use std::path::PathBuf;
 
 /// The ELF (executable and linkable format) file for the Succinct RISC-V zkVM.
-pub const FIBONACCI_ELF: &[u8] = include_elf!("fibonacci-program");
+pub const CHACHA_ELF: &[u8] = include_elf!("chacha-program");
 
 /// The arguments for the EVM command.
 #[derive(Parser, Debug)]
@@ -42,7 +40,7 @@ enum ProofSystem {
 /// A fixture that can be used to test the verification of SP1 zkVM proofs inside Solidity.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct SP1FibonacciProofFixture {
+struct SP1ChachaProofFixture {
     a: u32,
     b: u32,
     n: u32,
@@ -62,7 +60,7 @@ fn main() {
     let client = ProverClient::from_env();
 
     // Setup the program.
-    let (pk, vk) = client.setup(FIBONACCI_ELF);
+    let (pk, vk) = client.setup(CHACHA_ELF);
 
     // Setup the inputs.
     let mut stdin = SP1Stdin::new();
@@ -89,40 +87,39 @@ fn create_proof_fixture(
 ) {
     // Deserialize the public values.
     let bytes = proof.public_values.as_slice();
-    let PublicValuesStruct { n, a, b } = PublicValuesStruct::abi_decode(bytes, false).unwrap();
 
     // Create the testing fixture so we can test things end-to-end.
-    let fixture = SP1FibonacciProofFixture {
-        a,
-        b,
-        n,
-        vkey: vk.bytes32().to_string(),
-        public_values: format!("0x{}", hex::encode(bytes)),
-        proof: format!("0x{}", hex::encode(proof.bytes())),
-    };
+    // let fixture = SP1ChachaProofFixture {
+    //     a,
+    //     b,
+    //     n,
+    //     vkey: vk.bytes32().to_string(),
+    //     public_values: format!("0x{}", hex::encode(bytes)),
+    //     proof: format!("0x{}", hex::encode(proof.bytes())),
+    // };
 
     // The verification key is used to verify that the proof corresponds to the execution of the
     // program on the given input.
     //
     // Note that the verification key stays the same regardless of the input.
-    println!("Verification Key: {}", fixture.vkey);
+    // println!("Verification Key: {}", fixture.vkey);
 
     // The public values are the values which are publicly committed to by the zkVM.
     //
     // If you need to expose the inputs or outputs of your program, you should commit them in
     // the public values.
-    println!("Public Values: {}", fixture.public_values);
+    // println!("Public Values: {}", fixture.public_values);
 
     // The proof proves to the verifier that the program was executed with some inputs that led to
     // the give public values.
-    println!("Proof Bytes: {}", fixture.proof);
+    // println!("Proof Bytes: {}", fixture.proof);
 
     // Save the fixture to a file.
-    let fixture_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../contracts/src/fixtures");
-    std::fs::create_dir_all(&fixture_path).expect("failed to create fixture path");
-    std::fs::write(
-        fixture_path.join(format!("{:?}-fixture.json", system).to_lowercase()),
-        serde_json::to_string_pretty(&fixture).unwrap(),
-    )
-    .expect("failed to write fixture");
+    // let fixture_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../contracts/src/fixtures");
+    // std::fs::create_dir_all(&fixture_path).expect("failed to create fixture path");
+    // std::fs::write(
+    //     fixture_path.join(format!("{:?}-fixture.json", system).to_lowercase()),
+    //     serde_json::to_string_pretty(&fixture).unwrap(),
+    // )
+    // .expect("failed to write fixture");
 }
--- script/src/bin/main.rs
+++ script/src/bin/main.rs
@@ -10,13 +10,11 @@
 //! RUST_LOG=info cargo run --release -- --prove
 //! ```
 
-use alloy_sol_types::SolType;
 use clap::Parser;
-use fibonacci_lib::PublicValuesStruct;
 use sp1_sdk::{include_elf, ProverClient, SP1Stdin};
 
 /// The ELF (executable and linkable format) file for the Succinct RISC-V zkVM.
-pub const FIBONACCI_ELF: &[u8] = include_elf!("fibonacci-program");
+pub const CHACHA_ELF: &[u8] = include_elf!("chacha-program");
 
 /// The arguments for the command.
 #[derive(Parser, Debug)]
@@ -56,26 +54,22 @@ fn main() {
 
     if args.execute {
         // Execute the program
-        let (output, report) = client.execute(FIBONACCI_ELF, &stdin).run().unwrap();
+        let (output, report) = client.execute(CHACHA_ELF, &stdin).run().unwrap();
         println!("Program executed successfully.");
 
         // Read the output.
-        let decoded = PublicValuesStruct::abi_decode(output.as_slice(), true).unwrap();
-        let PublicValuesStruct { n, a, b } = decoded;
-        println!("n: {}", n);
-        println!("a: {}", a);
-        println!("b: {}", b);
+        let decoded = 0; //TODO
 
-        let (expected_a, expected_b) = fibonacci_lib::fibonacci(n);
-        assert_eq!(a, expected_a);
-        assert_eq!(b, expected_b);
-        println!("Values are correct!");
+        // let (expected_a, expected_b) = chacha_lib::chacha(n);
+        // assert_eq!(a, expected_a);
+        // assert_eq!(b, expected_b);
+        // println!("Values are correct!");
 
         // Record the number of cycles executed.
         println!("Number of cycles: {}", report.total_instruction_count());
     } else {
         // Setup the program for proving.
-        let (pk, vk) = client.setup(FIBONACCI_ELF);
+        let (pk, vk) = client.setup(CHACHA_ELF);
 
         // Generate the proof
         let proof = client
